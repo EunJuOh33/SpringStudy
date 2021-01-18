8bd5f34a1d4c326c8081453b5e197d749cca64ac
dooeun <58769961+dooeun@users.noreply.github.com>
2021-01-18 14:33:27 +0900

    [#8] feat : 댓글 수정 컨트롤러 메소드 작성

--- src/main/java/org/zerock/controller/ReplyController.java
+++ src/main/java/org/zerock/controller/ReplyController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.zerock.domain.Criteria;
 import org.zerock.domain.ReplyVO;
@@ -77,6 +78,22 @@ public class ReplyController {
 		log.info("remove" + rno);
 		
 		return service.remove(rno) == 1 ?
+				new ResponseEntity<>("success", HttpStatus.OK) : new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	/* 댓글 수정 */
+	@RequestMapping(method = { RequestMethod.PUT, RequestMethod.PATCH },
+					value = "/{rno}",
+					consumes = "application/json",
+					produces = { MediaType.TEXT_PLAIN_VALUE })
+	public ResponseEntity<String> modify(@RequestBody ReplyVO vo, @PathVariable("rno") Long rno) {
+		
+		vo.setRno(rno);
+		
+		log.info("rno: " + rno);
+		log.info("modify: " + vo);
+		
+		return service.modify(vo) == 1 ?
 				new ResponseEntity<>("success", HttpStatus.OK) : new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 }
