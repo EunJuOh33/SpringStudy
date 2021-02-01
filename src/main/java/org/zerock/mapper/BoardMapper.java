d41df5c7ce606de02bdf4597bc491bd87455caaf
EunJuOh33 <eunjuoh.dev@gmail.com>
2021-02-01 20:10:22 +0900

    [#18] feat : 댓글 수 칼럼 테이블에 추가, 이에 따른 프로젝트 수정

--- src/main/java/org/zerock/mapper/BoardMapper.java
+++ src/main/java/org/zerock/mapper/BoardMapper.java
@@ -2,6 +2,7 @@ package org.zerock.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
 import org.zerock.domain.BoardVO;
 import org.zerock.domain.Criteria;
 
@@ -22,4 +23,6 @@ public interface BoardMapper {
 	List<BoardVO> getListWithPaging(Criteria cri);
 	
 	int getTotalCount(Criteria cri);
+	
+	public void updateReplyCnt(@Param("bno") Long bno, @Param("amount") int amount);
 }
