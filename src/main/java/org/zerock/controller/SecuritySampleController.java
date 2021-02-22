5dbd8af441a7db844f82519d30a677d9d6292221
EunJuOh33 <eunjuoh.dev@gmail.com>
2021-02-22 22:27:07 +0900

    [#23] env : 지우고 다시 수정

---
+++ src/main/java/org/zerock/controller/SecuritySampleController.java
@@ -0,0 +1,31 @@
+package org.zerock.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@RequestMapping("/samplesecurity/*")
+@Log4j
+public class SecuritySampleController {
+
+/* Spring Security 연습 */
+	
+	@GetMapping("/all")
+	public void doAll() {
+		log.info("do all can access everybody");
+	}
+	
+	@GetMapping("/member")
+	public void doMember() {
+		log.info("logined member");
+	}
+	
+	@GetMapping("/admin")
+	public void doAdmin() {
+		log.info("admin only");
+	}
+	
+}
