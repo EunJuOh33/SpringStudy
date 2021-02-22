711f1c941257e2cbe8352b3293d91351a11198ed
EunJuOh33 <eunjuoh.dev@gmail.com>
2021-02-22 18:58:33 +0900

    [#23] env : SecurityConfig 자바 설정. xml 설정 연습필요

---
+++ src/main/java/org/zerock/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package org.zerock.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import lombok.extern.log4j.Log4j;
+
+@Configuration
+@EnableWebSecurity
+@Log4j
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		
+		http.authorizeRequests()
+			.antMatchers("/sample/all").permitAll()
+			.antMatchers("/sample/admin").access("hasRole('ROLE_ADMIN')")
+			.antMatchers("/sample/member").access("hasRole('ROLE_MEMBER')");
+		
+		http.formLogin().loginPage("/customLogin").loginProcessingUrl("/login");
+		
+	}
+	
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		log.info("configure....................");
+		auth.inMemoryAuthentication().withUser("admin").password("{noop}admin").roles("ADMIN");
+		auth.inMemoryAuthentication().withUser("member").password("{noop}member").roles("MEMBER");
+	}
+	
+}
